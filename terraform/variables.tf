82dcfbab355ce8a87f63332095c3246023f5b170
Julien BONACHERA <julien.bonachera@smile.fr>
2016-09-03 13:43:07 +0200

    add dns variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,11 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_region" {}
 variable "lb_auth_token" {}
+variable "zone_id" {}
+variable "zone" {}
+variable "nomad_cname" {
+    default = "nomad"
+}
+variable "service_cname" {
+    default = "*.app"
+}
