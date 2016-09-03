87eb59cc0f0a641d4f7d9073fd82e6f0f6a5f214
Julien BONACHERA <julien.bonachera@smile.fr>
2016-09-03 22:17:18 +0200

    add zone and zone_id vars to entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -14,6 +14,8 @@ export TF_VAR_aws_access_key=${aws_access_key:-$(askvar aws_access_key)}
 export TF_VAR_aws_secret_key=${aws_secret_key:-$(askvar aws_secret_key)}
 export TF_VAR_aws_region=${aws_region:-$(askvar aws_region)}
 export TF_VAR_lb_auth_token=${aws_region:-$(askvar lb_auth_token)}
+export TF_VAR_zone=${aws_region:-$(askvar zone)}
+export TF_VAR_zone_id=${aws_region:-$(askvar zone_id)}
 
 echo "running $@"
 make $@
